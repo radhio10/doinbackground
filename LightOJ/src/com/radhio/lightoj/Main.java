f5aa7afda452de07bf9eb48449f5ec42e766c9a0
radhio10 <azmiahaqueradio@gmail.com>
2021-03-27 22:55:01 +0600

    Initial Commit

---
+++ LightOJ/src/com/radhio/lightoj/Main.java
@@ -0,0 +1,15 @@
+package com.radhio.lightoj;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCase = scanner.nextInt();
+        for (int i = 0; i < testCase; i++) {
+            
+        }
+        System.out.println(testCase);
+    }
+}
